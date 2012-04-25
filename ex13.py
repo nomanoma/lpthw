5a7f11a3a29216f0792c2a857ec19897b936a024
noma <noma@>
2012-04-26 00:43:21 +0400

    ex13, 14, 15.5

--- ex13.py
+++ ex13.py
@@ -6,3 +6,8 @@ print "This script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+
+test = raw_input("gimme gimme!! ")
+
+print "1 %r, 2 %r, 3 %r, 4 %r, 5 %s" % (
+    script, first, second, third, test)
