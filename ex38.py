f6fa7ad635d58004532a55ea2edf3c7cba95a38f
noma <noma@>
2012-07-31 18:23:34 +0400

    ex38 started

---
+++ ex38.py
@@ -0,0 +1,20 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print "Wait there's not 10 things in that list, let's fix that."
+
+stuff = ten_things.split(' ')               # split(' ', stuff)
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()             # pop(' ', more_stuff)
+    print "Adding: ", next_one
+    stuff.append(next_one)                  # append('next_one', 'stuff')
+    print "There's %d items now." % len(stuff)
+
+print "There we go: ", stuff
+
+print stuff[1]
+print stuff[-1]
+print stuff.pop()                           # pop(stuff)
+print ' '.join(stuff) # what? cool!         # join(' ', stuff)
+print '#'.join(stuff[3:5]) # super stellar! # join('#', stuff)
